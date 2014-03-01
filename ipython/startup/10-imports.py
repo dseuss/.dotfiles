afad59c35c493ba9fd68ec22343c03c04cda6728
Daniel Suess <daniel.suess@web.de>
2014-03-01 14:54:44 +1000

    Ipython configuration added

---
+++ ipython/startup/10-imports.py
@@ -0,0 +1,14 @@
+from __future__ import print_function, division
+
+# Start scipy libaries
+import numpy as np
+import matplotlib.pyplot as pl
+
+# enable LaTeX printing for sympy
+from IPython.display import display
+from sympy.interactive import printing
+printing.init_printing()
+
+from IPython.core.pylabtools import figsize
+figsize(10, 4)
+
