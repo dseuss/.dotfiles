406a7d2eb12cb76b35fd273c8403e369977bd8c4
Daniel Suess <daniel.suess@web.de>
2014-03-02 12:16:38 +1000

    Interactive plots in notebook using mlpd3

--- ipython/startup/10-imports.py
+++ ipython/startup/10-imports.py
@@ -6,9 +6,19 @@ import matplotlib.pyplot as pl
 
 # enable LaTeX printing for sympy
 from IPython.display import display
-from sympy.interactive import printing
-printing.init_printing()
+try:
+   from sympy.interactive import printing
+   printing.init_printing()
+except ImportError:
+   pass
 
+# Set figsize on the fly
 from IPython.core.pylabtools import figsize
 figsize(10, 4)
 
+# Use mpld3 to display interactive graphs
+try:
+   import mpld3
+   mpld3.enable_notebook()
+except ImportError:
+   pass
