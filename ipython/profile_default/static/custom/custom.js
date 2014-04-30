573960b6fdf68a616413b52c7023962c53f134fc
Daniel Suess <daniel.suess@web.de>
2014-04-30 15:33:42 +0200

    Disable codemirror vim

--- ipython/profile_default/static/custom/custom.js
+++ ipython/profile_default/static/custom/custom.js
@@ -1,8 +1,8 @@
-$.getScript("/static/components/codemirror/keymap/vim.js", function() {
-    if (! IPython.Cell) return;
-    IPython.Cell.options_default.cm_config.keyMap = "vim";
-});
+// $.getScript("/static/components/codemirror/keymap/vim.js", function() {
+//     if (! IPython.Cell) return;
+//     IPython.Cell.options_default.cm_config.keyMap = "vim";
+// });
 
-require(["nbextensions/vim"], function (vim_extension) {
-    vim_extension.load_extension();
-});
+// require(["nbextensions/vim"], function (vim_extension) {
+//     vim_extension.load_extension();
+// });
