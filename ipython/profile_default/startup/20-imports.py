1920a536c7eb7b2fa95b5d7bd38051b02c25df1f
Daniel Suess <daniel.suess@web.de>
2015-07-17 10:51:52 +0200

    Matplotlib 1.4.3 has styles -- no need for seaborn

--- ipython/profile_default/startup/20-imports.py
+++ ipython/profile_default/startup/20-imports.py
@@ -4,21 +4,8 @@ del sys
 # Start scipy libaries
 import numpy as np
 import matplotlib.pyplot as pl
-
-# enable LaTeX printing for sympy
-from IPython.display import display
-try:
-    from sympy.interactive import printing
-    printing.init_printing()
-except ImportError:
-    pass
-
-try:
-    import seaborn as sns
-    sns.set_style('darkgrid')
-except ImportError:
-    pass
+pl.style.use('ggplot')
 
 # Set figsize on the fly
 from IPython.core.pylabtools import figsize
-figsize(15, 5)
+figsize(1.618 * 5, 5)
