8ca2f6b36e5d7f342cc50bb959484d7836de697c
Daniel Suess <daniel@dsuess.me>
2017-04-21 10:57:27 +1000

    Cleanup PATH

--- vars.sh
+++ vars.sh
@@ -1,9 +1,6 @@
-export PYTHONPATH=~/Code/Pythonlibs:$PYTHONPATH
-export GOPATH=~/Code/Go/
-export PATH=~/bin/:$GOPATH/bin:~/.cabal/bin:~/Library/Miniconda3/bin:~/.cargo/bin:$PATH
 export EDITOR=vim
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
-export MOSEKLM_LICENSE_FILE=$HOME/Library/mosek/mosek.lic
-export RUST_SRC_PATH=/usr/local/src/rust/src
-PATH=$PATH:$HOME/.local/bin
+
+export PYTHONPATH=~/Code/Pythonlibs:$PYTHONPATH
+export PATH=$HOME/Library/Miniconda3/bin:$HOME/.local/bin/:/usr/local/opt/llvm/bin:$PATH
