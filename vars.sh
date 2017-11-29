a3d748891d51bb7169b1068bf048a69ba59aff39
Daniel Suess <daniel@dsuess.me>
2017-11-29 11:57:18 +0100

    Add ~/bin to PATH

--- vars.sh
+++ vars.sh
@@ -1,13 +1,15 @@
-export EDITOR=nvim
+export EDITOR=/usr/local/bin/nvim
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 
-export PATH=$HOME/Library/Miniconda3/bin:$HOME/.local/bin/:/usr/local/opt/llvm/bin:$PATH
+export PATH=$HOME/bin/:$HOME/Library/Miniconda3/bin:$HOME/.local/bin/:$HOME/.cargo/bin/:/usr/local/opt/llvm/bin:$PATH
 
 export PYTHONPATH=~/Code/Pythonlibs:$PYTHONPATH
 export TF_CPP_MIN_LOG_LEVEL=3
 
 export CC=clang
 export CXX=clang++
+
+export RUST_SRC_PATH=$(rustc --print sysroot)/lib/rustlib/src/rust/src
 
 export FZF_DEFAULT_COMMAND='fd --type f'
