c39a1181ba111941811263faf4b312a24f085a69
Daniel Suess <daniel.suess@web.de>
2019-01-07 10:15:35 +1100

    Update LD_LIBRARY_PATH

--- vars.sh
+++ vars.sh
@@ -1,2 +1,3 @@
 # add shell variables to this file
 export PATH=$HOME/bin:~/.local/conda/bin/:~/.local/bin/:~/.linuxbrew/bin/:$PATH
+export LD_LIBRARY_PATH=$HOME/.local/cuda/lib64:$HOME/.local/TensorRT-5.0.2.6/lib:$LD_LIBRARY_PATH
